c606304f5caf760053282dfadb90ca92040f3531
Juan Velez Ballesteros <juanvelezballesteros@gmail.com>
2015-02-21 09:00:42 -0600

    se suben los ejerciciso

---
+++ Touch/Touch/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  Touch
+//
+//  Created by Juan Velez Ballesteros on 14/02/15.
+//  Copyright (c) 2015 Juan Velez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *metodo;
+@property (strong, nonatomic) IBOutlet UILabel *estatus;
+@property (strong, nonatomic) IBOutlet UILabel *tapStatus;
+
+
+@end
+
---
+++ collection/collection/CollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.m
+//  collection
+//
+//  Created by Juan Velez Ballesteros on 21/02/15.
+//  Copyright (c) 2015 Juan Velez. All rights reserved.
+//
+
+#import "CollectionViewCell.h"
+
+@implementation CollectionViewCell
+
+@end
